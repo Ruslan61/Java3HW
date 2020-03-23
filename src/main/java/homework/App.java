704485bf28e41e3792f866554b948441bc2c2b1f
Ruslan61 <ruslan.musaev.91@mail.ru>
2020-03-24 00:09:12 +0300

    Выполнил ДЗ

---
+++ src/main/java/homework/App.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class App {
+
+    public static void main(String[] args) {
+        Array array = new Array();
+        int[] arrOne = {1, 2, 4, 4, 2, 3, 4, 1, 7};
+        array.arrayCheck(arrOne);
+        System.out.println(array.booleanCheck(arrOne));
+    }
+
+}
